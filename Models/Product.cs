c354f432732c0d6d1de4e1b0fbc87a8a104775f8
Kenny-Wouters <kendev007@outlook.com>
2025-10-16 11:22:57 +0200

    Enhance product data binding functionality
    - Updated `BindProp.razor` to improve product display and interaction:
      - Display "Active"/"Inactive" for `IsActive` property.
      - Added `Price` property with two-way binding for input.
      - Added checkbox for toggling `IsActive` with two-way binding.
      - Added dropdown to select and display product properties dynamically.
      - Introduced `selectedProp` field for managing selected property.
    
    - Updated `Product.cs`:
      - Added `Price` property to represent product price.
      - Added `ProductProperties` to store product attributes.
    
    - Added `Product_Prop.cs`:
      - Defined `Product_Prop` class to represent product attributes.

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,5 +6,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
+        public int Price { get; set; }
+
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
     }
 }
